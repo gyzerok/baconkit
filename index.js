e0d47d078844f087d343b0491106e4dcfbb89a14
Fedor Nezhivoy <gyzerok@gmail.com>
2014-12-08 23:58:34 +0600

    added Bacon to the root object

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+    Bacon: require('baconjs').Bacon,
     Object: require('./lib/Object'),
     Application: require('./lib/Application'),
     Window: require('./lib/Window')
